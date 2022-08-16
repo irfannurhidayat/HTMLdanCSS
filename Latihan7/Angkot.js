dabc721e6d8957ded18ff45bbe41d5025b5098b2
irfannurhidayat <110875546+irfannurhidayat@users.noreply.github.com>
2022-08-16 12:07:45 +0700

    Add files via upload

---
+++ Latihan7/Angkot.js
@@ -0,0 +1,9 @@
+for (var nilaiAwal = 1 ; nilaiAwal <=10 ; nilaiAwal++) {
+    if (nilaiAwal <=7) {
+        console.log('Angkot No. ' + nilaiAwal + ' beroperasi dengan baik.')
+    } else if (nilaiAwal >=9){
+        console.log('Angkot No. ' + nilaiAwal + ' sedang tidak beroperasi.')
+    }else{
+        console.log('Angkot No. ' + nilaiAwal + ' sedang lembur.')
+    }
+}
